d196866ab738b817a6548141108883562932e1b3
SantiagoLeal24 <santiagolealwhyetcetc@gmail.com>
2026-05-03 23:42:45 -0300

    Salto y RunWall
    Mecanica de Salto y Runwall

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -14,54 +14,103 @@ public class playerMovement : MonoBehaviour
     public float tiempoSuavizadoGiro = 0.1f;
     private float velocidadGiroActual;
 
-    [Header("Gravedad")]
+    [Header("Gravedad y Salto")]
     public float gravedad = -9.81f;
     private Vector3 velocidadCaida;
+    public float fuerzaSalto = 2f;
+    public float multiplicadorImpulsoFrontal = 1.5f;
+
+    [Header("Chequeo de Suelo")]
     public Transform chequeoSuelo;
     public float distanciaSuelo = 0.4f;
     public LayerMask mascaraSuelo;
     private bool enSuelo;
 
+    [Header("Wall Run")]
+    public LayerMask mascaraPared;
+    public float distanciaRaycatPared = 0.6f;
+    public float gravedadWallRun = -1f; //gravedad reducida
+    public bool tocandoParedIzquierda;
+    private bool tocandoParedDerecha;
+    private bool enWallRun;
+
+
     void Update()
     {
-        // 1. Chequear suelo
+        ChequearSuelo();
+        Movimiento();
+        Salto();
+        aplicarGravedad();
+        ChequearParedes();
+    }    
+
+    private void ChequearSuelo()
+    {
         enSuelo = Physics.CheckSphere(chequeoSuelo.position, distanciaSuelo, mascaraSuelo);
 
-        if (enSuelo && velocidadCaida.y < 0)
+        if (enSuelo && velocidadCaida.y  < 0)
         {
             velocidadCaida.y = -2f;
         }
+    }
 
-        // 2. Leer Input
+    private void ChequearParedes()
+    {
+        tocandoParedDerecha = Physics.Raycast(transform.position, transform.right, distanciaRaycatPared, mascaraPared);
+        tocandoParedIzquierda = Physics.Raycast(transform.position, -transform.right, distanciaRaycatPared, mascaraPared);
+        enWallRun = (tocandoParedDerecha || tocandoParedIzquierda) && !enSuelo;
 
+    }
+
+    private void Movimiento()
+    {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direccion = new Vector3(horizontal, 0f, vertical).normalized;
 
-        // 3. Mover y rotar con Input
-
         if (direccion.magnitude >= 0.1f)
         {
-
-            // Calcula el ángulo hacia donde queremos ir, sumando la rotación de la cámara
             float anguloObjetivo = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
-            // Suaviza la transición entre el ángulo actual y el objetivo
             float angulo = Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloObjetivo, ref velocidadGiroActual, tiempoSuavizadoGiro);
             transform.rotation = Quaternion.Euler(0f, angulo, 0f);
 
-            // Convierte el ángulo en una dirección hacia la que moverse
             Vector3 direccionMovimiento = Quaternion.Euler(0f, anguloObjetivo, 0f) * Vector3.forward;
 
-            // Aplica el movimiento horizontal
-            controller.Move(direccionMovimiento.normalized * velocidad * Time.deltaTime);
-        
+            // Si estamos en el aire aplicamos multiplicador para salto mas largo
+            float velocidadActual = enSuelo ? velocidad : velocidad * multiplicadorImpulsoFrontal;
+
+            controller.Move(direccionMovimiento.normalized * velocidadActual * Time.deltaTime);
+        }
     }
 
-        // Gravedad
+    private void Salto()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (enSuelo)
+            {
+                velocidadCaida.y = Mathf.Sqrt(fuerzaSalto * -2f * gravedad);
+            }
+            else if (enWallRun)
+            {
+                velocidadCaida.y = Mathf.Sqrt(fuerzaSalto * -2f * gravedad);
+            }
 
-        velocidadCaida.y += gravedad * Time.deltaTime;
+        }
+    }
+
+    private void aplicarGravedad()
+    {
+        if (enWallRun)
+        {
+            velocidadCaida.y = gravedadWallRun;
+        }
+        else
+        {
+            velocidadCaida.y += gravedad * Time.deltaTime;
+        }
+            
         controller.Move(velocidadCaida * Time.deltaTime);
     }
 }
